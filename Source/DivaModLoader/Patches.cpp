1ec31c805b5773a66a405b1887cab1b7a0cdbccb
MikuLover-2007 <maximchern.2007@gmail.com>
2026-07-20 23:32:35 +0200

    Update Patches.cpp

--- Source/DivaModLoader/Patches.cpp
+++ Source/DivaModLoader/Patches.cpp
@@ -77,10 +77,10 @@ void Patches::init()
 
     // Remove module ID limit of 1035.
     WRITE_MEMORY((uint8_t*)sigModuleIdLimit1() + 0xD, uint8_t, 0x89, 0xD0, 0xC3);
-    WRITE_NOP((uint8_t*)sigModuleIdLimit2() + 0xB, 4);
+    //WRITE_NOP((uint8_t*)sigModuleIdLimit2() + 0xB, 4);
 
     // Remove COS limit of 498.
-    WRITE_MEMORY(sigCosLimit1(), uint8_t, 0xEB);
+    //WRITE_MEMORY(sigCosLimit1(), uint8_t, 0xEB);
     //WRITE_NOP(sigCosLimit2(), 4);
 
     // Remove PV DB date check.
