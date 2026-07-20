3c96bd3f2da3197d3a0465067d3b03be4cd698f6
MikuLover-2007 <maximchern.2007@gmail.com>
2026-07-20 23:39:54 +0200

    Update Patches.cpp

--- Source/DivaModLoader/Patches.cpp
+++ Source/DivaModLoader/Patches.cpp
@@ -76,8 +76,8 @@ void Patches::init()
     WRITE_MEMORY((uint8_t*)sigRomCheck2() + 0x12, uint8_t, 0xEB);
 
     // Remove module ID limit of 1035.
-    WRITE_MEMORY((uint8_t*)sigModuleIdLimit1() + 0xD, uint8_t, 0x89, 0xD0, 0xC3);
-    //WRITE_NOP((uint8_t*)sigModuleIdLimit2() + 0xB, 4);
+    //WRITE_MEMORY((uint8_t*)sigModuleIdLimit1() + 0xD, uint8_t, 0x89, 0xD0, 0xC3);
+    WRITE_NOP((uint8_t*)sigModuleIdLimit2() + 0xB, 4);
 
     // Remove COS limit of 498.
     //WRITE_MEMORY(sigCosLimit1(), uint8_t, 0xEB);
