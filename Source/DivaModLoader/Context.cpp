a4b3c1fa8df85119e44b7181da85775ebed2dcc0
MikuLover-2007 <maximchern.2007@gmail.com>
2026-07-20 22:18:43 +0200

    Update Context.cpp

--- Source/DivaModLoader/Context.cpp
+++ Source/DivaModLoader/Context.cpp
@@ -91,7 +91,7 @@ void Context::init()
         freopen("CONOUT$", "w", stdout);
     }
 
-    //Patches::init();
+    Patches::init();
     ModLoader::init();
     CodeLoader::init();
     FileLoader::init();
@@ -106,6 +106,6 @@ void Context::init()
 void Context::postInit()
 {
     CodeLoader::postInit();
-    PvLoader::init();
+    //PvLoader::init();
     ThumbnailLoader::init();
 }
